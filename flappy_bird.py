818980724291fdf127b3ce0ddbaf5a96115bf7ef
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-10-24 15:37:28 +0800

    Add pipe spawning and creation logic
    Introduced pipe sprite loading, creation function, and timed spawning using a custom event. Adjusted bird movement on space key press and refactored variable organization for clarity.

--- flappy_bird.py
+++ flappy_bird.py
@@ -14,9 +14,14 @@ clock = pygame.time.Clock()
 
 #functions
 def draw_floor():
-     screen_surface.blit(floor, (floor_x_pos,850))
-     screen_surface.blit(floor, (floor_x_pos + 576,850))
+    screen_surface.blit(floor, (floor_x_pos,850))
+    screen_surface.blit(floor, (floor_x_pos + 576,850))
+    pass
 
+
+def create_pipe():
+    new_pipe = pipe_surface.get_rect(midtop = (300, 512))
+    return new_pipe
 #variables
 gravity = 0.1
 bird_movement = 0
@@ -26,14 +31,20 @@ bg_surface = pygame.image.load("./assests/sprites/background-day.png")
 bg_surface = pygame.transform.scale2x(bg_surface)
 
 
-
+#floor
 floor = pygame.image.load("./assests/sprites/base.png")
 floor = pygame.transform.scale2x(floor)
 floor_x_pos = 0
-
+#bird
 bird_surface = pygame.image.load("./assests/sprites/redbird-midflap.png")
 bird_surface = pygame.transform.scale2x(bird_surface)
 bird_rect = bird_surface.get_rect(center = (200,200))
+#pipe
+pipe_surface = pygame.image.load("./assests/sprites/pipe-green.png")
+pipe_surface = pygame.transform.scale2x(pipe_surface)
+pipe_list = []
+SPAWN_PIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWN_PIPE, 1500)
 # GAME LOOP
 # (has methods to draw)
 # (checks for user input)
@@ -47,9 +58,12 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
-                bird_movement -= 10
+                bird_movement -= 5
                 pass
             pass
+        if event.type == SPAWN_PIPE:
+            pipe_list.append(create_pipe())
+            print(pipe_list)
         pass
     screen_surface.blit(bg_surface, (0, 0))
 
