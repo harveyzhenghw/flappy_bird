77b812c2397629bebff8b43dce27d82e6933c354
harveyzhenghw <harvey.zheng.hw@gmail.com>
2025-10-24 15:05:47 +0800

    Initial commit

---
+++ flappy_bird.py
@@ -0,0 +1,75 @@
+# INITIALIZE PYGAME
+# # Import the pygame module
+import pygame,sys
+# Initialize module
+pygame.init()
+pygame.display.set_caption("Flappy Bird by Harvey")
+
+# DISPLAY SURFACE
+# (has width and height)
+SCREEN_WIDTH = 576
+SCREEN_HEIGHT = 1024
+screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+
+#functions
+def draw_floor():
+     screen_surface.blit(floor, (floor_x_pos,850))
+     screen_surface.blit(floor, (floor_x_pos + 576,850))
+
+#variables
+gravity = 0.1
+bird_movement = 0
+
+
+bg_surface = pygame.image.load("./assests/sprites/background-day.png")
+bg_surface = pygame.transform.scale2x(bg_surface)
+
+
+
+floor = pygame.image.load("./assests/sprites/base.png")
+floor = pygame.transform.scale2x(floor)
+floor_x_pos = 0
+
+bird_surface = pygame.image.load("./assests/sprites/redbird-midflap.png")
+bird_surface = pygame.transform.scale2x(bird_surface)
+bird_rect = bird_surface.get_rect(center = (200,200))
+# GAME LOOP
+# (has methods to draw)
+# (checks for user input)
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            sys.exit()
+            pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = 0
+                bird_movement -= 10
+                pass
+            pass
+        pass
+    screen_surface.blit(bg_surface, (0, 0))
+
+    bird_movement += gravity
+    bird_rect.centery += bird_movement
+    screen_surface.blit(bird_surface,bird_rect)
+
+
+    
+
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
+    
+
+    pygame.display.update()
+    clock.tick(120)
+
+
+
+# QUIT PYGAME
